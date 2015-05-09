3599a0b3453cbdfc9eac9cb7e904a6b06b2ab165
wei <mavenwei@qq.com>
2015-05-09 14:24:22 +0800

    接收端口参数

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var http = require('http');
 var rf = require("fs");
 var server = new http.Server();
-
+var port=process.argv[2]==null?"10000":process.argv.splice(2);
 server.on('request', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(getPacText());
 });
-
 var pac = "";
 function getPacText() {
     if (pac == "") {
-        pac = rf.readFileSync("20150509.pac", "utf-8");
-        console.log(pac);
+        pac = rf.readFileSync("20150509pac", "utf-8");
+        console.log("pac file loaded.");
     }
     return pac;
 }
-server.listen(10000);
+if(!isNaN(parseInt(port,10))){
+    server.listen(10000);
+    console.log("Server is online.")
+}else{
+    console.log("Server start fail.")
+
+}
 
 
